830c454dfae43e698fd23dba2a514762b6ced7bf
Yang-cola <1549029765@qq.com>
2023-03-22 14:53:02 +0800

    request

---
+++ flaskL/requestL.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')   #渲染register.html文件
+
+@app.route('/get_data')
+def get_data():
+    return '提交成功'
+if __name__ == '__main__':
+    app.run(port=8080,debug=True)
