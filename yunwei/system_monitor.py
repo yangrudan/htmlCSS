c7e03ce72ae3d3290769c240b04f456b5e47ef17
Yang-cola <1549029765@qq.com>
2023-05-29 15:50:35 +0800

    flask后端向前端推送数据

--- yunwei/system_monitor.py
+++ yunwei/system_monitor.py
@@ -17,3 +17,8 @@ print("--------------------------------")
 print(psutil.disk_partitions())
 print("--------------------------------")
 print(psutil.net_if_addrs())
+
+x = y =3
+print("xx:", x is y)
+x1 = x2 = [1.,2,3]
+print(x1 is x2)
