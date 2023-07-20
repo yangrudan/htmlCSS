3665fa6ac57edb7e64813e50bece4dcd30fb893d
Yang-cola <1549029765@qq.com>
2023-07-20 16:32:35 +0800

    psutil

--- yunwei/system_monitor.py
+++ yunwei/system_monitor.py
@@ -1,24 +1,20 @@
 import psutil
 
-print(psutil.cpu_times())
+print(psutil.cpu_times())  # 统计 CPU 的用户／系统／空闲时间
 print("--------------------------------")
 print(psutil.cpu_times(percpu=True))
 print("--------------------------------")
-print(psutil.cpu_count())
+print(psutil.cpu_count())  # 获取 CPU 的逻辑核心数量
 print("--------------------------------")
-print(psutil.cpu_count(logical=False))
+print(psutil.cpu_count(logical=False))  # 获取 CPU 的物理核心数量
 print("--------------------------------")
-print(psutil.cpu_percent())
+print(psutil.cpu_percent())  # 查看 CPU 的使用率
 print("--------------------------------")
 print(psutil.cpu_percent(percpu=True))
 print("--------------------------------")
-print(psutil.virtual_memory())
+print(psutil.virtual_memory())  # 查看内存使用情况
 print("--------------------------------")
-print(psutil.disk_partitions())
+print(psutil.disk_partitions())  # 查看磁盘分区、磁盘使用率和磁盘 IO 信息
 print("--------------------------------")
 print(psutil.net_if_addrs())
 
-x = y =3
-print("xx:", x is y)
-x1 = x2 = [1.,2,3]
-print(x1 is x2)
